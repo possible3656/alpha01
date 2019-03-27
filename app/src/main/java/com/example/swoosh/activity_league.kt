4a9c84efbfde84762b5da58fa5eef54efc44d292
RAUNAK <sj94611@gmail.com>
2019-03-28 04:19:06 +0530

    third one

--- app/src/main/java/com/example/swoosh/activity_league.kt
+++ app/src/main/java/com/example/swoosh/activity_league.kt
@@ -1,13 +1,19 @@
 package com.example.swoosh
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_league.*
 
 class activity_league : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+        next.setOnClickListener {
+            val newvar = Intent(this,level::class.java)
+            startActivity(newvar)
+        }
 
     }
 }
